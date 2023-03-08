82a66e84fda7edb507787d23170944b644b86bd0
alexi55 <76796913+alexi55@users.noreply.github.com>
2023-03-08 15:42:59 +0100

    quitado public

--- resources/views/archivos2/btn2.blade.php
+++ resources/views/archivos2/btn2.blade.php
@@ -4,7 +4,7 @@
     <td style="padding: 0;" class="text-center p-1">
         @can('archivos_view')
             <span class="tts:left tts-slideIn tts-custom" aria-label="Previsualizar">
-                <a href="/sai/public/Documentos/{{$documento}}" target="blank_">
+                <a href="../Documentos/{{$documento}}" target="blank_">
                     <span class="text-primary" >
                         <i class="fa fa-eye fa-lg" style="color:rgb(87, 58, 231)"></i>
                     </span>
