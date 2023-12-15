8be12b85822f5d495a5b006864e394eeb7bdde66
wimigo03 <wimigo03@gmail.com>
2023-12-15 17:38:53 -0400

    actualizar usuario

--- routes/Admin/users-route.php
+++ routes/Admin/users-route.php
@@ -7,7 +7,7 @@ Route::prefix('users')->name('users.')->middleware(['auth'])->group(function () 
     Route::get('/create', 'Admin\UserController@create')->name('create')->middleware('can:users.create');
     Route::post('/store', 'Admin\UserController@store')->name('store')->middleware('can:users.create');
     Route::get('/edit/{id}', 'Admin\UserController@edit')->name('edit')->middleware('can:users.editar');
-    Route::put('/update', 'Admin\UserController@update')->name('update')->middleware('can:users.editar');
+    Route::post('/update', 'Admin\UserController@update')->name('update')->middleware('can:users.editar');
     Route::get('/baja/{id}', 'Admin\UserController@baja')->name('baja')->middleware('can:users.deshabilitar');
     Route::get('/alta/{id}', 'Admin\UserController@alta')->name('alta')->middleware('can:users.habilitar');
 });
